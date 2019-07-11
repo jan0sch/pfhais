6a864e01893defd8e8558bff2c5377c363f5d943
Jens Grassel <jens@wegtam.com>
2019-07-11 17:23:36 +0200

    Pure
    * prepare abstract routes

--- pure/src/main/scala/com/wegtam/books/pfhais/pure/api/ProductRoutes.scala
+++ pure/src/main/scala/com/wegtam/books/pfhais/pure/api/ProductRoutes.scala
@@ -11,25 +11,25 @@
 
 package com.wegtam.books.pfhais.pure.api
 
-import cats.Monad
+import cats.{ Applicative, Monad }
 import cats.effect.Sync
-//import cats.implicits._
+import cats.implicits._
 import com.wegtam.books.pfhais.pure.db._
-//import com.wegtam.books.pfhais.pure.models._
-//import eu.timepit.refined.auto._
+import com.wegtam.books.pfhais.pure.models._
+import eu.timepit.refined.auto._
 import org.http4s._
-//import org.http4s.circe._
+import org.http4s.circe._
 import org.http4s.dsl._
 
 final class ProductRoutes[F[_]: Monad: Sync](repo: Repository[F]) extends Http4sDsl[F] {
+  implicit def wtf[A[_]: Applicative]: EntityEncoder[A, Option[Product]] = jsonEncoderOf
 
   val routes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "product" / UUIDVar(id) =>
-      //for {
-      //  rows <- repo.loadProduct(id)
-      //  resp <- Ok(Product.fromDatabase(rows))
-      //} yield resp
-      ???
+      for {
+        rows <- repo.loadProduct(id)
+        resp <- Ok(Product.fromDatabase(rows))
+      } yield resp
     case PUT -> Root / "product" / UUIDVar(id) =>
       ???
   }
