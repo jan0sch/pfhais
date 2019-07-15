635ca00180c89b9567bdc2482862edc346da8aad
Jens Grassel <jens@wegtam.com>
2019-07-15 08:16:22 +0200

    Pure
    * products routes

--- pure/src/main/scala/com/wegtam/books/pfhais/pure/api/ProductsRoutes.scala
+++ pure/src/main/scala/com/wegtam/books/pfhais/pure/api/ProductsRoutes.scala
@@ -11,18 +11,29 @@
 
 package com.wegtam.books.pfhais.pure.api
 
+import cats.Applicative
 import cats.effect.Sync
+import cats.implicits._
 import com.wegtam.books.pfhais.pure.db._
+import com.wegtam.books.pfhais.pure.models._
+import eu.timepit.refined.auto._
 import org.http4s._
+import org.http4s.circe._
 import org.http4s.dsl._
 
 final class ProductsRoutes[F[_]: Sync](repo: Repository[F]) extends Http4sDsl[F] {
+  implicit def decodeProduct: EntityDecoder[F, Product]                            = jsonOf
+  implicit def encodeProduct[A[_]: Applicative]: EntityEncoder[A, Option[Product]] = jsonEncoderOf
 
   val productsRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "products" =>
       ???
-    case POST -> Root / "products" =>
-      ???
+    case req @ POST -> Root / "products" =>
+      for {
+        p <- req.as[Product]
+        _ <- repo.saveProduct(p)
+        r <- NoContent()
+      } yield r
   }
 
 }
