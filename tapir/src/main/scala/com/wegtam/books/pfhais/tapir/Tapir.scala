0102ac229dfd7fe16fbfa53ea80931c2d08bdf9a
Jens Grassel <jens@wegtam.com>
2019-11-10 11:01:08 +0100

    Tapir
    * add missing modifications
    * Code Cleanup

--- tapir/src/main/scala/com/wegtam/books/pfhais/tapir/Tapir.scala
+++ tapir/src/main/scala/com/wegtam/books/pfhais/tapir/Tapir.scala
@@ -28,6 +28,7 @@ import org.http4s.implicits._
 import org.http4s.server.Router
 import org.http4s.server.blaze._
 import pureconfig._
+import shapeless.Witness
 import tapir.docs.openapi._
 import tapir.openapi._
 import tapir.openapi.circe.yaml._
@@ -89,12 +90,22 @@ object Tapir extends IOApp {
   }
 
   /**
+    * Extract the regular expression value from an existing
+    * implicit witness of for the type `String`.
+    *
+    * @return The value of the Witness.
+    */
+  def extractRegEx[S <: String](implicit ws: Witness.Aux[S]): String =
+    ws.value
+
+  /**
     * Update the provided documentation structure by adding some information.
     *
     * @param docs The generated OpenAPI documentation from tapir.
     * @return An updated documentation structure.
     */
   private def updateDocumentation(docs: OpenAPI): OpenAPI = {
+    // Define needed type class instances for Monocle
     implicit def atListMap[K, V]: At[ListMap[K, V], K, Option[V]] = At(
       i => Lens((_: ListMap[K, V]).get(i))(optV => map => optV.fold(map - i)(v => map + (i -> v)))
     )
@@ -110,26 +121,21 @@ object Tapir extends IOApp {
     implicit def listMapEach[K, V]: Each[ListMap[K, V], V] =
       Each(listMapTraversal)
     // Generate some lenses.
+    val components: Lens[OpenAPI, Option[Components]] = GenLens[OpenAPI](_.components)
+    val componentsSchemas: Lens[Components, ListMap[String, OpenAPI.ReferenceOr[Schema]]] =
+      GenLens[Components](_.schemas)
     val paths: Lens[OpenAPI, ListMap[String, PathItem]] = GenLens[OpenAPI](_.paths)
-    val deleteOps: Lens[PathItem, Option[Operation]]    = GenLens[PathItem](_.delete)
     val getOps: Lens[PathItem, Option[Operation]]       = GenLens[PathItem](_.get)
-    val postOps: Lens[PathItem, Option[Operation]]      = GenLens[PathItem](_.post)
     val putOps: Lens[PathItem, Option[Operation]]       = GenLens[PathItem](_.put)
     val operationParams: Lens[Operation, List[OpenAPI.ReferenceOr[Parameter]]] =
       GenLens[Operation](_.parameters)
-    val operationResponses: Lens[Operation, ListMap[ResponsesKey, OpenAPI.ReferenceOr[Response]]] =
-      GenLens[Operation](_.responses)
-    val responseContent: Lens[Response, ListMap[String, MediaType]] =
-      GenLens[Response](_.content)
-    val mediaTypeSchema: Lens[MediaType, Option[OpenAPI.ReferenceOr[Schema]]] =
-      GenLens[MediaType](_.schema)
-    val pathParams: Lens[PathItem, List[OpenAPI.ReferenceOr[Parameter]]] =
-      GenLens[PathItem](_.parameters)
     val parameterSchema: Lens[Parameter, OpenAPI.ReferenceOr[Schema]] = GenLens[Parameter](_.schema)
     val schemaProperties: Lens[Schema, ListMap[String, OpenAPI.ReferenceOr[Schema]]] =
       GenLens[Schema](_.properties)
     val schemaPattern: Lens[Schema, Option[String]] = GenLens[Schema](_.pattern)
     // Now try to get things going...
+    //val langRegex = extractRegEx[LanguageCode]
+    val langRegex = "/^[a-z]{2}$/"
     val uuidRegex = "/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i"
     val updateGetProductId =
       (paths composeLens at("/product/{id}") composeOptional possible composeLens getOps composeOptional possible composeLens operationParams composeTraversal each composeOptional possible composeLens parameterSchema composeOptional possible composeLens schemaPattern)
@@ -137,10 +143,18 @@ object Tapir extends IOApp {
     val updatePutProductId =
       (paths composeLens at("/product/{id}") composeOptional possible composeLens putOps composeOptional possible composeLens operationParams composeTraversal each composeOptional possible composeLens parameterSchema composeOptional possible composeLens schemaPattern)
         .set(uuidRegex.some)(updateGetProductId)
-    val _ =
-      (paths composeTraversal each composeLens getOps composeOptional possible composeLens operationResponses composeTraversal each composeOptional possible composeLens responseContent composeLens at(
-        "Product"
-      ) composeOptional possible composeLens mediaTypeSchema).getAll(docs)
-    updatePutProductId
+    val updateModelProduct =
+      (components composeOptional possible composeLens componentsSchemas composeLens at("Product") composeOptional possible composeOptional possible composeLens schemaProperties composeLens at(
+        "id"
+      ) composeOptional possible composeOptional possible composeLens schemaPattern)
+        .set(uuidRegex.some)(updatePutProductId)
+    val updateModelTranslation =
+      (components composeOptional possible composeLens componentsSchemas composeLens at(
+        "Translation"
+      ) composeOptional possible composeOptional possible composeLens schemaProperties composeLens at(
+        "lang"
+      ) composeOptional possible composeOptional possible composeLens schemaPattern)
+        .set(langRegex.some)(updateModelProduct)
+    updateModelTranslation
   }
 }
