0454580b9e609cf9898376a89264747ae28d1b22
Jens Grassel <jens@wegtam.com>
2022-02-18 14:05:15 +0100

    Fix main entry point omitting optics

--- tapir-scala-3/src/main/scala/com/wegtam/books/pfhais/tapir/Tapir.scala
+++ tapir-scala-3/src/main/scala/com/wegtam/books/pfhais/tapir/Tapir.scala
@@ -14,31 +14,25 @@ package com.wegtam.books.pfhais.tapir
 import java.util.concurrent.{ ExecutorService, Executors }
 
 import cats.effect._
-import cats.implicits._
 import com.typesafe.config._
 import com.wegtam.books.pfhais.tapir.api._
 import com.wegtam.books.pfhais.tapir.config._
 import com.wegtam.books.pfhais.tapir.db._
-import com.wegtam.books.pfhais.tapir.models.LanguageCode
 import doobie._
 import eu.timepit.refined.auto._
-import monocle._
-import monocle.function.all._
-import monocle.macros.GenLens
-import org.http4s.implicits._
-import org.http4s.server.Router
 import org.http4s.ember.server._
+import org.http4s.server.Router
 import pureconfig._
 import sttp.tapir.docs.openapi._
 import sttp.tapir.openapi._
 import sttp.tapir.openapi.circe.yaml._
-import sttp.tapir.swagger.http4s.SwaggerHttp4s
+import sttp.tapir.server.http4s.Http4sServerInterpreter
+import sttp.tapir.swagger.SwaggerUI
 
-import scala.collection.immutable._
 import scala.concurrent.ExecutionContext
 import scala.io.StdIn
 
-object Tapir extends IOApp.WithContext {
+object Tapir extends IOApp {
   val availableProcessors: Int      = Runtime.getRuntime().availableProcessors() / 2
   val blockingCores: Int            = if (availableProcessors < 2) 2 else availableProcessors
   val blockingPool: ExecutorService = Executors.newFixedThreadPool(blockingCores)
@@ -49,10 +43,9 @@ object Tapir extends IOApp.WithContext {
 
   @SuppressWarnings(Array("scalafix:DisableSyntax.null"))
   def run(args: List[String]): IO[ExitCode] = {
-    val blocker                        = Blocker.liftExecutorService(blockingPool)
     val migrator: DatabaseMigrator[IO] = new FlywayDatabaseMigrator
 
-    val program = for {
+    for {
       (apiConfig, dbConfig) <- IO {
         val cfg = ConfigFactory.load(getClass().getClassLoader())
         // TODO Think about alternatives to `Throw`.
@@ -67,36 +60,28 @@ object Tapir extends IOApp.WithContext {
       repo           = new DoobieRepository(tx)
       productRoutes  = new ProductRoutes(repo)
       productsRoutes = new ProductsRoutes(repo)
-      docs = List(
-        ProductRoutes.getProduct,
-        ProductRoutes.updateProduct,
-        ProductsRoutes.getProducts,
-        ProductsRoutes.createProduct
-      ).toOpenAPI("Pure Tapir API", "1.0.0")
+      docs = OpenAPIDocsInterpreter().toOpenAPI(
+        List(
+          ProductRoutes.getProduct,
+          ProductRoutes.updateProduct,
+          ProductsRoutes.getProducts,
+          ProductsRoutes.createProduct
+        ),
+        "Pure Tapir API",
+        "1.0.0"
+      )
       updatedDocs = updateDocumentation(docs)
-      docsRoutes  = new SwaggerHttp4s(updatedDocs.toYaml)
+      docsRoutes  = Http4sServerInterpreter[IO]().toRoutes(SwaggerUI[IO](updatedDocs.toYaml))
       routes      = productRoutes.routes <+> productsRoutes.routes
-      httpApp     = Router("/" -> routes, "/docs" -> docsRoutes.routes).orNotFound
+      httpApp     = Router("/" -> routes, "/docs" -> docsRoutes).orNotFound
       resource = EmberServerBuilder
         .default[IO]
-        .withBlocker(blocker)
         .withHost(apiConfig.host)
         .withPort(apiConfig.port)
         .withHttpApp(httpApp)
         .build
-      fiber = resource.use(_ => IO(StdIn.readLine())).as(ExitCode.Success)
+      fiber <- resource.use(_ => IO(StdIn.readLine())).as(ExitCode.Success)
     } yield fiber
-    program.attempt.unsafeRunSync() match {
-      case Left(e) =>
-        IO {
-          println("*** An error occured! ***")
-          if (e ne null) {
-            println(e.getMessage)
-          }
-          ExitCode.Error
-        }
-      case Right(r) => r
-    }
   }
 
   /**
@@ -105,36 +90,36 @@ object Tapir extends IOApp.WithContext {
     * @param docs The generated OpenAPI documentation from tapir.
     * @return An updated documentation structure.
     */
-  private def updateDocumentation(docs: OpenAPI): OpenAPI = {
+  private def updateDocumentation(docs: OpenAPI): OpenAPI =
     // Generate some lenses.
-    val components: Lens[OpenAPI, Option[Components]] = GenLens[OpenAPI](_.components)
-    val componentsSchemas: Lens[Components, ListMap[String, OpenAPI.ReferenceOr[Schema]]] =
-      GenLens[Components](_.schemas)
-    val paths: Lens[OpenAPI, ListMap[String, PathItem]] = GenLens[OpenAPI](_.paths)
-    val getOps: Lens[PathItem, Option[Operation]]       = GenLens[PathItem](_.get)
-    val putOps: Lens[PathItem, Option[Operation]]       = GenLens[PathItem](_.put)
-    val operationParams: Lens[Operation, List[OpenAPI.ReferenceOr[Parameter]]] =
-      GenLens[Operation](_.parameters)
-    val parameterSchema: Lens[Parameter, OpenAPI.ReferenceOr[Schema]] = GenLens[Parameter](_.schema)
-    val schemaProperties: Lens[Schema, ListMap[String, OpenAPI.ReferenceOr[Schema]]] =
-      GenLens[Schema](_.properties)
-    val schemaPattern: Lens[Schema, Option[String]] = GenLens[Schema](_.pattern)
-    // Now try to get things going...
-    val typeRegex = implicitly[RefinedExtract[LanguageCode]].regex
-    val langRegex = "/" + typeRegex + "/" // convert to Javascript regular expression
-    val uuidRegex = "/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i"
-    val updateGetProductId =
-      (paths at ("/product/{id}") composeOptional possible andThen getOps composeOptional possible andThen operationParams composeTraversal each composeOptional possible andThen parameterSchema composeOptional possible andThen schemaPattern)
-        .replace(uuidRegex.some)(docs)
-    val updatePutProductId =
-      (paths at ("/product/{id}") composeOptional possible andThen putOps composeOptional possible andThen operationParams composeTraversal each composeOptional possible andThen parameterSchema composeOptional possible andThen schemaPattern)
-        .replace(uuidRegex.some)(updateGetProductId)
-    val updateModelProduct =
-      (components composeOptional possible andThen componentsSchemas at ("Product") composeOptional possible composeOptional possible andThen schemaProperties at ("id") composeOptional possible composeOptional possible andThen schemaPattern)
-        .replace(uuidRegex.some)(updatePutProductId)
-    val updateModelTranslation =
-      (components composeOptional possible andThen componentsSchemas at ("Translation") composeOptional possible composeOptional possible andThen schemaProperties at ("lang") composeOptional possible composeOptional possible andThen schemaPattern)
-        .replace(langRegex.some)(updateModelProduct)
-    updateModelTranslation
-  }
+    //val components: Lens[OpenAPI, Option[Components]] = GenLens[OpenAPI](_.components)
+    //val componentsSchemas: Lens[Components, ListMap[String, ReferenceOr[Schema]]] =
+    //  GenLens[Components](_.schemas)
+    //val paths: Lens[OpenAPI, ListMap[String, PathItem]] = GenLens[OpenAPI](_.paths)
+    //val getOps: Lens[PathItem, Option[Operation]]       = GenLens[PathItem](_.get)
+    //val putOps: Lens[PathItem, Option[Operation]]       = GenLens[PathItem](_.put)
+    //val operationParams: Lens[Operation, List[ReferenceOr[Parameter]]] =
+    //  GenLens[Operation](_.parameters)
+    //val parameterSchema: Lens[Parameter, ReferenceOr[Schema]] = GenLens[Parameter](_.schema)
+    //val schemaProperties: Lens[Schema, ListMap[String, ReferenceOr[Schema]]] =
+    //  GenLens[Schema](_.properties)
+    //val schemaPattern: Lens[Schema, Option[String]] = GenLens[Schema](_.pattern)
+    //// Now try to get things going...
+    //val typeRegex = implicitly[RefinedExtract[LanguageCode]].regex
+    //val langRegex = "/" + typeRegex + "/" // convert to Javascript regular expression
+    //val uuidRegex = "/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i"
+    //val updateGetProductId =
+    //  (paths at ("/product/{id}") composeOptional possible andThen getOps composeOptional possible andThen operationParams composeTraversal each composeOptional possible andThen parameterSchema composeOptional possible andThen schemaPattern)
+    //    .replace(uuidRegex.some)(docs)
+    //val updatePutProductId =
+    //  (paths at ("/product/{id}") composeOptional possible andThen putOps composeOptional possible andThen operationParams composeTraversal each composeOptional possible andThen parameterSchema composeOptional possible andThen schemaPattern)
+    //    .replace(uuidRegex.some)(updateGetProductId)
+    //val updateModelProduct =
+    //  (components composeOptional possible andThen componentsSchemas at ("Product") composeOptional possible composeOptional possible andThen schemaProperties at ("id") composeOptional possible composeOptional possible andThen schemaPattern)
+    //    .replace(uuidRegex.some)(updatePutProductId)
+    //val updateModelTranslation =
+    //  (components composeOptional possible andThen componentsSchemas at ("Translation") composeOptional possible composeOptional possible andThen schemaProperties at ("lang") composeOptional possible composeOptional possible andThen schemaPattern)
+    //    .replace(langRegex.some)(updateModelProduct)
+    //updateModelTranslation
+    docs
 }
