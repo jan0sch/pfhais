c2d4e95cd28396386b73f12b1de9917b30d42bb2
Jens Grassel <jens@wegtam.com>
2022-02-23 13:00:58 +0100

    Start Monocle

--- tapir-scala-3/src/main/scala/com/wegtam/books/pfhais/tapir/Tapir.scala
+++ tapir-scala-3/src/main/scala/com/wegtam/books/pfhais/tapir/Tapir.scala
@@ -19,8 +19,12 @@ import com.typesafe.config._
 import com.wegtam.books.pfhais.tapir.api._
 import com.wegtam.books.pfhais.tapir.config._
 import com.wegtam.books.pfhais.tapir.db._
+import com.wegtam.books.pfhais.tapir.models.LanguageCode
 import doobie._
 import eu.timepit.refined.auto._
+import monocle._
+import monocle.macros._
+import monocle.syntax.all._
 import org.http4s.ember.server._
 import org.http4s.server.Router
 import pureconfig._
@@ -84,36 +88,35 @@ object Tapir extends IOApp {
     * @return
     *   An updated documentation structure.
     */
-  private def updateDocumentation(docs: OpenAPI): OpenAPI =
+  private def updateDocumentation(docs: OpenAPI): OpenAPI = {
     // Generate some lenses.
     // val components: Lens[OpenAPI, Option[Components]] = GenLens[OpenAPI](_.components)
     // val componentsSchemas: Lens[Components, ListMap[String, ReferenceOr[Schema]]] =
-    //  GenLens[Components](_.schemas)
+    // GenLens[Components](_.schemas)
     // val paths: Lens[OpenAPI, ListMap[String, PathItem]] = GenLens[OpenAPI](_.paths)
     // val getOps: Lens[PathItem, Option[Operation]]       = GenLens[PathItem](_.get)
     // val putOps: Lens[PathItem, Option[Operation]]       = GenLens[PathItem](_.put)
     // val operationParams: Lens[Operation, List[ReferenceOr[Parameter]]] =
-    //  GenLens[Operation](_.parameters)
+    // GenLens[Operation](_.parameters)
     // val parameterSchema: Lens[Parameter, ReferenceOr[Schema]] = GenLens[Parameter](_.schema)
     // val schemaProperties: Lens[Schema, ListMap[String, ReferenceOr[Schema]]] =
-    //  GenLens[Schema](_.properties)
+    // GenLens[Schema](_.properties)
     // val schemaPattern: Lens[Schema, Option[String]] = GenLens[Schema](_.pattern)
-    //// Now try to get things going...
-    // val typeRegex = implicitly[RefinedExtract[LanguageCode]].regex
-    // val langRegex = "/" + typeRegex + "/" // convert to Javascript regular expression
-    // val uuidRegex = "/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i"
-    // val updateGetProductId =
-    //  (paths at ("/product/{id}") composeOptional possible andThen getOps composeOptional possible andThen operationParams composeTraversal each composeOptional possible andThen parameterSchema composeOptional possible andThen schemaPattern)
-    //    .replace(uuidRegex.some)(docs)
+    // Now try to get things going...
+    val langRegex = "/^[a-z]{2}$/"
+    val uuidRegex = "/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i"
+    // (paths at ("/product/{id}") composeOptional possible andThen getOps composeOptional possible andThen operationParams composeTraversal each composeOptional possible andThen parameterSchema composeOptional possible andThen schemaPattern)
+    //   .replace(uuidRegex.some)(docs)
     // val updatePutProductId =
-    //  (paths at ("/product/{id}") composeOptional possible andThen putOps composeOptional possible andThen operationParams composeTraversal each composeOptional possible andThen parameterSchema composeOptional possible andThen schemaPattern)
-    //    .replace(uuidRegex.some)(updateGetProductId)
+    // (paths at ("/product/{id}") composeOptional possible andThen putOps composeOptional possible andThen operationParams composeTraversal each composeOptional possible andThen parameterSchema composeOptional possible andThen schemaPattern)
+    //   .replace(uuidRegex.some)(updateGetProductId)
     // val updateModelProduct =
-    //  (components composeOptional possible andThen componentsSchemas at ("Product") composeOptional possible composeOptional possible andThen schemaProperties at ("id") composeOptional possible composeOptional possible andThen schemaPattern)
-    //    .replace(uuidRegex.some)(updatePutProductId)
+    // (components composeOptional possible andThen componentsSchemas at ("Product") composeOptional possible composeOptional possible andThen schemaProperties at ("id") composeOptional possible composeOptional possible andThen schemaPattern)
+    //   .replace(uuidRegex.some)(updatePutProductId)
     // val updateModelTranslation =
-    //  (components composeOptional possible andThen componentsSchemas at ("Translation") composeOptional possible composeOptional possible andThen schemaProperties at ("lang") composeOptional possible composeOptional possible andThen schemaPattern)
-    //    .replace(langRegex.some)(updateModelProduct)
+    // (components composeOptional possible andThen componentsSchemas at ("Translation") composeOptional possible composeOptional possible andThen schemaProperties at ("lang") composeOptional possible composeOptional possible andThen schemaPattern)
+    //   .replace(langRegex.some)(updateModelProduct)
     // updateModelTranslation
     docs
+  }
 }
